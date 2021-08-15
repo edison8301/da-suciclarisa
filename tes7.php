00ac4d34164e5f126e7137c58a310e82f28f7dea
Suci Clarisa Sundari <suciclarisa.s@gmail.com>
2021-08-15 10:35:50 +0700

    tes7

---
+++ tes7.php
@@ -0,0 +1,14 @@
+<?php
+
+$kalimat1="Selamat";
+$kalimat2="pagi pagi";
+
+$hasil1=strlen($kalimat1);
+$hasil2=strlen($kalimat2);
+
+echo "Jumlah karakter 1: $hasil1";
+echo "<br>";
+echo "Jumlah karakter 2: $hasil2";
+
+
+?>
