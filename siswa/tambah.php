5ff2b68b2ab75a488dea03f2b51cdf45a6d212e9
Suci Clarisa Sundari <suciclarisa.s@gmail.com>
2021-08-29 11:33:10 +0700

    html

--- siswa/tambah.php
+++ siswa/tambah.php
@@ -1,11 +1,11 @@
 <form action=tambah-proses.php method="get">
 <a href="tes14.php">Tambah Data Siswa</a><br/>
 	Nama<br>
-	<input name="nama"></br>
+	<input name="nama"><br/>
 	NISN<br>
-	<input name="nisn"></br>
+	<input name="nisn"><br/>
 	Alamat<br>
-	<input name="alamat"><br>
+	<input name="alamat"><br/>
 
 	<button>Kirim</button>
 
